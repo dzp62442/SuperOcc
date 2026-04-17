1ff5d930f5d06df431da746152301511a03c4bad
dzp62442 <dzp62442@163.com>
2026-04-17 07:11:25 +0000

    A100跑通

---
+++ haoce_activate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source /vepfs-mlp2/c20250502/haoce/dzp/activate-cuda.sh
+
+source /vepfs-mlp2/c20250502/haoce/dzp/activate-conda.sh superocc
+
+source /vepfs-mlp2/c20250502/haoce/dzp/activate-ai.sh
+
+source /vepfs-mlp2/c20250502/haoce/dzp/activate-git.sh
+
+source /vepfs-mlp2/c20250502/haoce/dzp/.wandb_cfg
